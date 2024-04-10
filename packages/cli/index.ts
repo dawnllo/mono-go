cbb6c917be0687920de727ab3b74fe00dad923a5
dawnl <1686661978@qq.com>
2024-04-10 23:28:28 +0800

    dlc 起步

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,25 +1,20 @@
 #!/usr/bin/env node
-import { Command } from 'commander' // 定义指令
-import chalk from 'chalk' // 字体颜色
+import { Command } from 'commander'
+import chalk from 'chalk'
 import excuteQueues from './process/index.js'
-// import inquirer from 'inquirer' // 采集用户输入
-// import ora from 'ora' // loading动效
-// import handlebars from 'handlebars'// 模板引擎
-// import fs from 'fs'
 
-const mcli = new Command()
+const dlc = new Command()
 
-// 命令的描述
-mcli
-  .name(chalk.green('mcli'))
+dlc
+  .name(chalk.green('dlc'))
   .description('study build myself Cli Tool !')
   .version(chalk.green('1.0.0'), '-v, --version', 'version')
   .option('-h, --help', 'help information')
 
-mcli
+dlc
   .command('add <template> [rename]')
   .description('add template')
   .option('-f, --force', 'force overwrite file destination !!!')
   .action(excuteQueues)
 
-mcli.parse()
+dlc.parse()
