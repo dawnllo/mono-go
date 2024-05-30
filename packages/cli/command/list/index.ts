a37acd5dc0ef70fb3270c4052e276b1ed4ee8018
dawnl <1686661978@qq.com>
2024-05-30 23:28:24 +0800

    go

--- packages/cli/command/list/index.ts
+++ packages/cli/command/list/index.ts
@@ -1,10 +1,9 @@
-import { cwd } from 'node:process'
-import path from 'node:path'
-import fs from 'node:fs'
+import prompts from 'prompts'
 import chalk from 'chalk'
 import ora from 'ora'
-import log from '../../utils/log'
 import http from '../../utils/http'
+import log from '../../utils/log'
+import { generateCatalog } from '../../utils'
 
 export default async function getListAction() {
   const spinner = ora('fetching...').start()
@@ -14,13 +13,46 @@ export default async function getListAction() {
     repo: 'vs-theme',
     type: _Global.GitFetchType.trees,
     branch: 'master',
-    recursive: true,
+    recursive: false,
   }
 
   const res = await http.git(config)
   const json = await res.json()
-
-  const tempDest = path.join(cwd(), 'temp.json')
-  fs.writeFileSync(tempDest, JSON.stringify(json))
   spinner.succeed(chalk.green('fetching success'))
+  const catalog = generateCatalog(json.tree)
+
+  const promptsConfig = [{
+    type: 'autocomplete',
+    name: 'url',
+    message: 'please input filter keyword, then press enter to select!',
+    choices: catalog.map((item) => {
+      return {
+        title: item.fileName,
+        value: item.url,
+      }
+    }),
+    async suggest(input, choices) {
+      return choices.filter((choice) => {
+        return choice.title.toLowerCase().includes(input.toLowerCase())
+      })
+    },
+    initial: 0,
+    fallback: 'no template exists in this repository!',
+  }, {
+    type: (url) => {
+      return url ? 'toggle' : null
+    },
+    name: 'confirm',
+    initial: false,
+    active: 'yes',
+    inactive: 'no',
+    message: 'can you confirm download!',
+  }]
+
+  // 交互
+  const result = await prompts(promptsConfig)
+
+  if (!result.confirm)
+    return log.red('download cancelled!')
+  // download
 }
--- packages/cli/utils/index.ts
+++ packages/cli/utils/index.ts
@@ -5,3 +5,35 @@ export function delay(timeout: number): Promise<void> {
     }, timeout)
   })
 }
+
+interface CatalogItem {
+  fileName: string
+  url: string
+  type: 'file' | 'dir'
+  size: number
+  children?: CatalogItem[]
+}
+export function generateCatalog(data, optionKeys = { fileName: 'path', url: 'url' }): CatalogItem[] {
+  if (!data)
+    return []
+
+  // 生成目录
+  function generate(data, optionKeys): CatalogItem[] {
+    const catalog: CatalogItem[] = []
+
+    for (const item of data) {
+      const ele: CatalogItem = {} as CatalogItem
+      if (item.children)
+        ele.children = generate(item.children, optionKeys)
+
+      catalog.push({
+        type: item.type === 'blob' ? 'file' : 'dir',
+        fileName: item[optionKeys.fileName],
+        size: item.size,
+        url: item.url,
+      })
+    }
+    return catalog
+  }
+  return generate(data, optionKeys)
+}
